538aae91f7594aeede234a7762d9ad7aad299717
Liv <110914989+livoszlak@users.noreply.github.com>
2024-02-26 11:16:56 +0100

    Added some Blade templates + a controller, updated login route in web.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<div>
+    @csrf
+    <form method="post" action="/login">
+        <div>
+            <label for="email">Email</label>
+            <input name="email" id="email" type="email" />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input name="password" id="password" type="password" />
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</div>
