1ab0a80b951f779ed22b70636a0375db2f03cc73
Liv <110914989+livoszlak@users.noreply.github.com>
2024-02-27 08:16:30 +0100

    Added LogoutController, fixed logout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('login')->middleware('guest');
 Route::post('login', LoginController::class);
+Route::post('logout', LogoutController::class);
 Route::get('dashboard', DashboardController::class)->middleware('auth');
