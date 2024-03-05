eb4dd7ef7255ab03050abc5723244a455e1dcded
Liv <110914989+livoszlak@users.noreply.github.com>
2024-03-05 13:09:56 +0100

    Track seeder and factory created with predefined list of track names and track lengths

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Track;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        $this->call(TrackSeeder::class);
     }
 }
