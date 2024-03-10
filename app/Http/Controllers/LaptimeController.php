60bb78de6d84bb82a1dee9536d9b701e0afec7ff
Liv <110914989+livoszlak@users.noreply.github.com>
2024-03-10 11:01:28 +0100

    Added throttle middleware (at 60 requests/min) both for guest and auth groups, consolidated all Laptime-related controllers into one controller

--- app/Http/Controllers/LaptimeController.php
+++ app/Http/Controllers/LaptimeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Laptime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LaptimeController extends Controller
 {
@@ -18,9 +20,39 @@ class LaptimeController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        try {
+            $messages = [
+                'car_id.required' => 'The car ID field must be filled in.',
+                'track_id.required' => 'The track field must be filled in.',
+                'date.required' => 'The date field must be filled in.',
+                'date.date' => 'The date must be a valid date.',
+                'time.required' => 'The time field must be filled in.',
+                'time.regex' => 'The time must be in the format mm:ss:msmsms.',
+            ];
+
+            $this->validate($request, [
+                'car_id' => 'required',
+                'track_id' => 'required',
+                'date' => 'required|date',
+                'time' => 'required|string|regex:/^\d{2}:\d{2}:\d{3}$/',
+            ], $messages);
+
+            $laptime = new Laptime;
+            $laptime->user_id = Auth::id();
+            $laptime->car_id = $request->car_id;
+            $laptime->track_id = $request->track_id;
+            $laptime->date = $request->date;
+            $laptime->time = $request->time;
+            $laptime->save();
+
+            return redirect('your-times')->with(['laptime' => $laptime]);
+        } catch (ValidationException $e) {
+            return redirect('register-time')->withErrors($e->errors());
+        } catch (\Exception $e) {
+            return redirect('register-time')->withErrors('An unexpected error occurred.');
+        }
     }
 
     /**
@@ -52,7 +84,33 @@ class LaptimeController extends Controller
      */
     public function update(Request $request, Laptime $laptime)
     {
-        //
+        $id = $request->input('laptime_id');
+
+        $messages = [
+            'date.required' => 'The date field must be filled in.',
+            'date.date' => 'The date must be a valid date.',
+            'time.required' => 'The time field must be filled in.',
+            'time.regex' => 'The time must be in the format mm:ss:msmsms.',
+        ];
+
+        $this->validate($request, [
+            'date' => 'required|date',
+            'time' => 'required|string|regex:/^\d{2}:\d{2}:\d{3}$/',
+            'track_id' => 'required',
+        ], $messages);
+
+        try {
+            $laptime = Laptime::find($id);
+            $laptime->date = $request->date;
+            $laptime->time = $request->time;
+            $laptime->track_id = $request->track_id;
+            $laptime->save();
+            return redirect('your-times')->with(['laptime' => $laptime]);
+        } catch (ValidationException $e) {
+            return redirect('your-times')->withErrors($e->errors());
+        } catch (\Exception $e) {
+            return redirect('your-times')->withErrors("An unexpected error occurred.");
+        }
     }
 
     /**
@@ -60,6 +118,7 @@ class LaptimeController extends Controller
      */
     public function destroy(Laptime $laptime)
     {
-        //
+        $laptime->delete();
+        return redirect('your-times');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use App\Http\Controllers\CarController;
-use App\Http\Controllers\CreateLaptimeController;
-use App\Http\Controllers\CreateUserController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\DeleteLaptimeController;
+use App\Http\Controllers\LaptimeController;
 use App\Http\Controllers\LeaderboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProfileShowController;
 use App\Http\Controllers\RegisterCarController;
 use App\Http\Controllers\RegisterTimeController;
-use App\Http\Controllers\UpdateLaptimeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\YourCarsController;
 use App\Http\Controllers\YourTimesController;
 use Illuminate\Support\Facades\Route;
@@ -29,25 +25,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware('guest')->group(function () {
+Route::middleware(['guest', 'throttle:60,1'])->group(function () {
     Route::view('/', 'index')->name('login');
     Route::view('register', 'register')->name('register');
     Route::view('login', 'login')->name('login');
     Route::post('login', LoginController::class);
-    Route::post('users', CreateUserController::class);
+    Route::post('users', [UserController::class, 'create']);
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'throttle:60,1'])->group(function () {
     Route::get('register-car', RegisterCarController::class);
     Route::get('register-time', RegisterTimeController::class);
     Route::get('your-cars', YourCarsController::class);
     Route::get('your-times', YourTimesController::class);
     Route::get('dashboard', DashboardController::class);
     Route::get('leaderboard/{track_id?}', LeaderboardController::class)->where('track_id', '[0-9]+')->defaults('track_id', 1);
+
     Route::post('logout', LogoutController::class);
-    Route::post('laptimes', CreateLaptimeController::class);
-    Route::post('laptimes/update', UpdateLaptimeController::class);
-    Route::patch('laptimes/{laptime}/delete', DeleteLaptimeController::class);
+    Route::post('laptimes', [LaptimeController::class, 'create']);
+    Route::post('laptimes/update', [LaptimeController::class, 'update']);
+    Route::patch('laptimes/{laptime}/delete', [LaptimeController::class, 'destroy']);
     Route::post('cars', [CarController::class, 'create']);
     Route::post('cars/update', [CarController::class, 'update']);
     Route::patch('cars/{car}/delete', [CarController::class, 'destroy']);
