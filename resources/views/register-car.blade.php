f3a596bd8b48c6f715af2f60f30927cc87438230
Marcus Heneborn <heneborn@gmail.com>
2024-03-11 16:38:37 +0100

    added styling
    svg buttons and mobile

--- resources/views/register-car.blade.php
+++ resources/views/register-car.blade.php
@@ -6,7 +6,8 @@
     <form class="form-container" action="cars" method="post">
         <h3>Add car</h3>
         @csrf
-        <label for="registration_number">Registration number </label>
+        <label for="registration_number">Registration #
+        </label>
         <input type="text" name="registration_number" id="registration_number">
         <label for="model">Model </label>
         <input type="text" name="model" id="model">
