3afdee618be4ae605e3c97b1799442547e1295a1
Marcus Heneborn <heneborn@gmail.com>
2024-03-11 23:54:06 +0100

    added styling
    overflow scroll to your-cars, hero image to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,14 @@
 @section('title', 'Car app')
 @section('content')
 
-<div class="hero-container"></div>
-    <div class="button-container">
-        <a href="/login" class="btn-btn-primary">Login</a>
-        <a href="/register" class="btn-btn-primary">Register</a>
-    </div>
-    
+<div class="hero-container">
+    <h1>Welcome to the car app</h1>
+    <img class="hero-image" src="{{url('assets/max-bottinger-0k_dCKxyIHc-unsplash.jpg')}}" alt="unavailable icon">
+</div>
+<div class="button-container">
+    <a href="/login" class="btn-btn-primary">Login</a>
+    <a href="/register" class="btn-btn-primary">Register</a>
+</div>
+
 @include('errors')
 @endsection
